1d54c8bc88e4cb8528257cc015a03d444c8610aa
neojarvis <techadmin@iamneo.ai>
2025-06-12 11:00:00 +0000

    Auto-save-commit-12/06/2025--16:29:52

--- hueinstall.sh
+++ hueinstall.sh
@@ -1,16 +1,24 @@
-# Install dependencies
+#!/bin/bash
+
+# Update and install dependencies
 sudo apt update
-sudo apt install -y git gcc g++ make python3-dev python3-venv libkrb5-dev libsasl2-dev libldap2-dev libssl-dev libmysqlclient-dev
+sudo apt install -y \
+  git gcc g++ make \
+  python3-dev python3-venv \
+  libkrb5-dev libsasl2-dev \
+  libldap2-dev libssl-dev \
+  libmariadb-dev-compat libmariadb-dev \
+  build-essential
 
-# Clone Hue source code
+# Clone the Hue repository
 git clone https://github.com/cloudera/hue.git
-cd hue
+cd hue || exit
 
-# Create virtualenv and activate
+# Create and activate a virtual environment
 python3 -m venv venv
 source venv/bin/activate
 
-# Install Python packages
+# Install Python packages and build Hue
 make apps
 
 # Start Hue
