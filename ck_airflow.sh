0e46bb3ff0cb2ba04eec3b1d555042c2c23c9015
neojarvis <techadmin@iamneo.ai>
2025-06-13 10:37:52 +0000

    Auto-save-commit-13/06/2025--16:07:47

--- ck_airflow.sh
+++ ck_airflow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "🔍 Checking Airflow..."
-airflow db migrate && airflow webserver --port 18080
+airflow db migrate && airflow webserver --port 8080
 
 # CLI Check
 airflow db check || echo "❌ Airflow DB not initialized"
